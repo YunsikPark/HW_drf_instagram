201f2df5234552b92dbde3c88f73a847612f651f
Yunsik Park <space214@gmail.com>
2017-07-10 22:23:56 +0900

    first commit | based on instagram project

---
+++ django_app/config/urls.py
@@ -0,0 +1,45 @@
+"""instagram URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.contrib import admin
+# from post import views as post_views
+from django.views.generic import RedirectView
+
+from config import settings, views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    # post앱의 urls.py 모듈을 include시킨다.
+    # url(r'^$', post_views.post_list),
+
+    # 새 view를 만들어 redirect시키는 방법
+    url(r'^$', views.index),
+
+    # 아래와 같이 쓰면 config폴더에 views.py를 만들지 않아도 된다
+    # Class-based View중 RedirectView를 사용하는 방법
+    # url(r'^$', RedirectView.as_view(pattern_name='post:post_list')),
+
+    # post앱의 urls.py모듈을 include시킨다.
+    url(r'^post/', include('post.urls')),
+    url(r'^member/', include('member.urls')),
+]
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
